ecb92d4b33b1939aff135b50367d108d35a65fee
munenyan1002 <sitaziki@gmail.com>
2018-05-23 03:10:58 +0900

    migrateし直した※migrateに失敗するのは、作成しようとしているファイルが既に「/db」上に存在しているから。ファイルを消してあげればmigrateエラーっぽい画面になるが、コマンドは成功してる

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_19_142141) do
+ActiveRecord::Schema.define(version: 2018_05_16_153424) do
 
   create_table "diaries", force: :cascade do |t|
     t.string "title"
