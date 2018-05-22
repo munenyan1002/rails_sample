111368b81ace985c412d91660571435e6f26ecc8
munenyan1002 <sitaziki@gmail.com>
2018-05-23 01:05:45 +0900

    ルーティングとJS・CSSを変更。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-
   def index
-    # render plain: 'Hello, world!'
     # コントローラーからビューを呼び出す
     @hello = 'Hello, world!'
     render template: 'users/index'
